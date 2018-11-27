5a95df20ddcfbeffb0f0251c4928a1a3adedf4c6
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2018-11-27 12:56:35 +0200

    fix counting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("My name is Ico. I am robot. I am . I cool. I go. I have food");
-        StringBuilder uniq = makeUniq(sb);
-        System.out.println(uniq);
+//        StringBuilder sb = new StringBuilder("My name is Ico. I am robot. am. I cool I. I go. I have food.");
+        StringBuilder sb = new StringBuilder("I. I am. Am. aM.");
+        String[] uniqueWords = makeUniq(sb);
+        /*System.out.println(uniq);
         System.out.println();
-        countWords(sb);
-
+        countWords(sb, uniq);*/
+        System.out.println(findMaxrepeated(sb, uniqueWords));
 
     }
 
@@ -49,7 +50,67 @@ public class Main {
         return ms + " " + max;
     }
 
-    private static void countWords(StringBuilder sb) {
+    private static String findMaxrepeated(StringBuilder sb, String[] uniqueWords) {
+        String[] sentences = sb.toString().replaceAll("[!]", ".")
+                .replaceAll("[...]", ".")
+                .replaceAll("[?]", ".")
+                .toLowerCase()
+                .split("[.]");
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].trim();
+            System.out.println(sentences[i]);
+        }
+
+        String[][] words = new String[sentences.length][sb.length()];
+        for(int i = 0; i < sentences.length; i++){
+            words[i] = sentences[i].split(" ");
+        }
+
+        int maxQuantityOfSentencesWithUniqueWord = 0;
+        String uniqueWordWithMaxQuantityOfSentencesIn = "";
+        for (String uniqueWord : uniqueWords) {
+            int quantityOfSentencesWithTheUniqueWord = 0;
+            for (String[] sentenseWords : words) {
+                for (String sentenseWord : sentenseWords) {
+                    if (sentenseWord.equals(uniqueWord)) {
+                        quantityOfSentencesWithTheUniqueWord++;
+                        break;
+                    }
+                }
+            }
+            if (quantityOfSentencesWithTheUniqueWord > maxQuantityOfSentencesWithUniqueWord) {
+                maxQuantityOfSentencesWithUniqueWord = quantityOfSentencesWithTheUniqueWord;
+                uniqueWordWithMaxQuantityOfSentencesIn = uniqueWord;
+            }
+        }
+
+        return uniqueWordWithMaxQuantityOfSentencesIn + ": " + maxQuantityOfSentencesWithUniqueWord;
+        /*StringBuilder[] sen = new StringBuilder[sentences.length];
+        for(int i = 0; i < sen.length; i++)
+            sen[i] = new StringBuilder(sentences[i]);
+
+
+        int max = 1;
+        int buff = 1;
+        String ms = "";
+        for (int i = 0; i < words.length; i++){
+            for (int j = 0; j < words[i].length; j++){
+                for (int k = 0; k < sen.length; k++){
+                    if (i == k) continue;
+                    if (sen[k].indexOf(words[i][j]) != -1) buff++;
+                }
+                if (max < buff){
+                    max = buff;
+                    ms = words[i][j];
+                }
+                buff = 1;
+            }
+        }
+        if (max == 1) return "not found repeates";
+        return ms + " " + max;*/
+    }
+
+    private static void countWords(StringBuilder sb, StringBuilder uniq) {
         char[] sentences = sb.toString().replaceAll("[!]", ".")
                 .replaceAll("[...]", ".")
                 .replaceAll("[?]", ".")
@@ -80,7 +141,7 @@ public class Main {
         System.out.println(res);
     }
 
-    private static StringBuilder makeUniq(StringBuilder sb) {
+    private static String[] makeUniq(StringBuilder sb) {
         String[] sentences = sb.toString().replaceAll("[!]", "")
                 .replaceAll("[...]", "")
                 .replaceAll("[?]", "")
@@ -98,11 +159,12 @@ public class Main {
                 }
             }
         }
-        StringBuilder uniq = new StringBuilder();
+        /*StringBuilder uniq = new StringBuilder();
         for (String s : result)
             if (s != null && !s.equals(""))
                 uniq.append(s + "\n");
 
-        return uniq;
+        return uniq;*/
+        return result;
     }
 }
